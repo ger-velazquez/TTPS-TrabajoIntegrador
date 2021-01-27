159dddefabf715bec6d9e1ac2293219679928821
Velazquez, German Nicolas <German Velazquez>
2021-01-26 23:52:31 -0300

    Still configurating the rails enviroment

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,7 @@
 class PageController < ApplicationController
-    def index
-    end
+  before_action :authenticate_user!
+
+  def index
+  end
 end
  
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,12 +7,13 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
