dc909855db13b383025c5cf305113b12e606ff1f
Velazquez, German Nicolas <German Velazquez>
2021-01-26 22:04:28 -0300

    Move rails app to root folder: step 1

--- lib/rn/modules.rb
+++
@@ -1,9 +0,0 @@
-module RN 
-    module Modules
-        autoload :Validations, 'rn/modules/module_validations'
-        autoload :Paths, 'rn/modules/module_paths'
-        autoload :Eliminator, 'rn/modules/module_eliminator'
-        autoload :Renderer, 'rn/modules/module_renderer'
-        autoload :Exporter, 'rn/modules/module_exporter'
-    end
-end
