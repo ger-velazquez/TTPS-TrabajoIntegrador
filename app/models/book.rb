0382514701fcdc358623adc3f02e0971288865ba
Velazquez, German Nicolas <German Velazquez>
2021-01-31 13:53:43 -0300

    Deleted currently model because it  will be crearted using scaffolding

--- app/models/book.rb
+++
@@ -1,4 +0,0 @@
-class Book < ApplicationRecord
-    has_many :notes
-    belongs_to :user
-end
---
+++ db/migrate/20210131165037_drop_book.rb
@@ -0,0 +1,5 @@
+class DropBook < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :books
+  end
+end
